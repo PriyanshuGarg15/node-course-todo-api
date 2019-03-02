330d04b0474dc7d70769fccd8810bfe87748fce9
Priyanshu Garg <punit1503@gmail.com>
2019-03-02 12:54:04 +0530

    Add Delete and find script of MongoDB

--- playground/mongo-find.js
+++ playground/mongo-find.js
@@ -7,5 +7,11 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
 
+  db.collection('Users').find({age: 25}).toArray().then((docs)=>{
+    console.log('Users: ');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err)=>{
+    console.log('err', err);
+  });
   client.close();
 });
