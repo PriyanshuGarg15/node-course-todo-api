58d45e2b05e5d3bf9fde49c0b30de24abd04f95b
Priyanshu Garg <punit1503@gmail.com>
2019-03-04 23:21:30 +0530

    some tweaking in DB connections

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise =  global.Promise;
-mongoose.connect('mongodb://priyanshu:password123@ds137703.mlab.com:37703/todo-api'||'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+var URL = 'mongodb://priyanshu:password123@ds137703.mlab.com:37703/todo-api'
+if(!process.env.PORT){
+  mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+}else{
+  mongoose.connect( URL , { useNewUrlParser: true });
+}
+
+
 
 
 module.exports ={mongoose};
