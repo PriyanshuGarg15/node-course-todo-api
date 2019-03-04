1aef140aece9f627754969663ce2f5267d782ff8
Priyanshu Garg <punit1503@gmail.com>
2019-03-04 16:06:42 +0530

    Add get todos specifically by :id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const {ObjectId}= require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} =require('./models/todo');
 var {User} = require('./models/user');
@@ -26,6 +27,21 @@ app.get("/todos", (req, res)=>{
     res.status(400).send(e);
   });
 });
+
+app.get('/todos/:id', (req, res)=>{ /////////////-------------Find Specific todo by ID
+  var id = req.params.id;
+  if(!ObjectId.isValid(id)){  //Check if Object Id is valid
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo)=>{   //Find the todo :if valid
+    if(!todo){
+      return res.status(404).send();   //if todo with the Id is not found
+    }
+    res.send(todo); //happy case: todo found
+  }).catch((e)=>{
+    res.status(404).send(); // catch any error
+  })
+})
 
 app.listen(3000,()=>{
   console.log('server is running at 3000');
