47f42fe5ec45addaafb20fdd609fa17c5dfe255f
Priyanshu Garg <punit1503@gmail.com>
2019-03-04 18:33:48 +0530

    Deploy to herokuu

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise =  global.Promise;
-mongoose.connect('mongodb://priyanshu:Qwerty@1234@ds137703.mlab.com:37703/todo-api'||'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect('mongodb://priyanshu:password123@ds137703.mlab.com:37703/todo-api'||'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 
 module.exports ={mongoose};
