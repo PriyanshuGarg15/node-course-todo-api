c4c24c0beb8a3cfc41c112cd2e1a56456df78e2a
Priyanshu Garg <punit1503@gmail.com>
2019-03-10 00:39:35 +0530

    Add tests for GET /users/me and POST /users route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -169,3 +169,62 @@ describe('PATCH /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('GET /users/me',()=>{
+  it('should return user if authenticated',(done)=>{
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  })
+
+  it('should return 401 if not authenticated', (done)=>{
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res)=>{
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  })
+});
+
+describe('POST /users',()=>{
+  it('should create a user', (done)=>{
+    var email= 'priyanshu@123.com';
+    var password= 'password123';
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(200)
+      .expect((res)=>{
+        expect(res.headers['x-auth']).toExist();
+        expect(res.body.email).toExist();
+        expect(res.body._id).toExist();
+      })
+      .end(done);   
+  });
+  it('should return validation error if request is invalid', (done)=>{
+    var email = 'abc123';
+    var password = 'fcvghbjnkl';
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(400)
+      .end(done);
+  });
+  it('should not create user if email in use', (done)=>{
+    var email= 'andrew@example.com';
+    var password = 'sdfbgffb';
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(400)
+      .end(done);
+  });
+})
